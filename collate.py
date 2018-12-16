560f362c304861e8eaeb6da130bf0846882b2b3a
David J Birnbaum <djbpitt@gmail.com>
2018-12-16 15:13:21 +0100

    create common substring table

--- collate.py
+++ collate.py
@@ -1,5 +1,8 @@
 import collections
 
+# Note
+# Single-token witnesses are not processed because they have no bigrams; this is an error
+
 # Sample data
 witnessData = {'wit1': ['a', 'b', 'c', 'd', 'e'], 'wit2': ['a', 'e', 'c', 'd'], 'wit3':['a', 'd', 'b']}
 
@@ -13,5 +16,15 @@ for key, value in witnessData.items():
         for second in range(first + 1, len(value)):
             csTable[value[first] + value[second]].append((key, first, second))
 
+# Sort table into common sequence list (csList)
+#   ordered by 1) number of witnesses (numerica high to low) and 2) sequence (alphabetic low to high)
+csList = [(k, csTable[k]) for k in sorted(csTable, key=lambda k: (-len(csTable[k]), k))]
+
+# Build topologically ordered list (toList)
+toList = []
+for bigram, locations in csList:
+    first, second = list(bigram)
+    print(first, second, (locations))
+
 # Diagnostic output
-print(csTable)
+# print(csList)
